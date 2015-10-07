910c45ffd1494a5ca5f71c844fb219ee6e05a918
cdt2132 <cdt2132@columbia.edu>
2015-10-07 18:33:14 -0400

    added javadoc comments to order class

--- Order.java
+++ Order.java
@@ -1,17 +1,31 @@
+/**The basic unit of this project, the order. Includes information 
+*like price, trader etc. and methods like print order
+*
+*@author Caroline Trimble 
+*@version 1 Build October 2015
+*/ 
+
 import java.util.Calendar;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public class Order{
 
+	/** The symbol, like "HH" for the order */
 	String symbol;
+	/** The date that the contract expires */
 	String contractEx;
+	/** Number of lots bought or sold */
 	int lots; 
+	/** Price per lot */
 	double price; 
+	/** Whether the order was bought or sold */
 	String buySell;
+	/** The first and last name of the trader*/
 	String trader; 
 
 	
+	/* The construction for an order simply assigns the parameters to the variables*/
 	public Order(String s, String c, int l, double p, String b, String t){
 		symbol = s; 
 		contractEx = c; 
@@ -21,6 +35,7 @@ public class Order{
 		trader = t; 
 	}
 
+	/*The print method prints the date and time of the order and all the information about the order*/
 	void printOrder(){
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Calendar cal = Calendar.getInstance();
