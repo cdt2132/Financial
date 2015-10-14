7fa7026a97bab0f4934c7ff337a61d69b92a82c8
sniffles1993 <haoxiang.gao@hotmail.com>
2015-10-14 01:09:36 -0400

    Added Test Class

---
+++ TestClass.java
@@ -0,0 +1,48 @@
+/**A class testing classes including Order, Display, Order, DatabaseManager
+*
+*@author Caroline Trimble, Kunal Jasty, Haoxiang Gao
+*@version 1 Build October 2015
+*/ 
+
+import java.sql.SQLException;
+
+public class TestClass {	
+	
+	public static void main(String[] args){
+		/** Test Order Class */
+		Order ord = new Order("BB",10,2015,1,30.0, 1, 1);
+		ord.printOrder();
+		System.out.println("Order Class OK");
+		
+		/** Test DatabaseManager Class*/
+		DatabaseManager db = new DatabaseManager();
+		db.insertOrder(ord);
+		System.out.println("DatabaseManager->insertOrder OK");
+		db.getResult("SELECT * FROM Orders ORDER BY ordertime DESC LIMIT 1");
+		try {
+			while (db.rs.next()){
+				for (int i = 1; i <= 8;i++)
+				System.out.println(db.rs.getString(i));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("DatabaseManager->getResult OK");
+		db.outputTrades("./");
+		System.out.println("DatabaseManager->outputTrades OK");
+		db.outputAggregate("./");
+		System.out.println("DatabaseManager->outputAggregate OK");		
+		
+		/** Test Save Dialog Class*/
+		SaveDialog svd = new SaveDialog();
+		System.out.println(svd.getName());
+		System.out.println("SaveDialog Class OK");
+		
+		/** Test Display Class */
+		Display disp = new Display();
+		System.out.println("Display class OK");
+		
+	}
+
+}
