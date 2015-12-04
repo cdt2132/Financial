319e23385fa1d6318ce3ee857999c3151fef3197
sniffles1993 <haoxiang.gao@hotmail.com>
2015-12-04 03:00:47 -0500

    Changed the member variables and constructor, deleted fixedorfloat, and added payer fixed, payer float

--- src/Swap.java
+++ src/Swap.java
@@ -8,27 +8,17 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Calendar;
 
 public class Swap {
-<<<<<<< HEAD
-	
 	/** Start day of swap*/
 	int startDay; 
-=======
-	/** Start day of swap */
-	int startDay;
->>>>>>> f96fcd41573cfe6e97eacfbb1a36b3b04b51262c
 	/** Start month of swap*/
 	int startMonth;
 	/** Start year of swap*/
 	int startYear;
-<<<<<<< HEAD
 	/** Termination day of swap*/
 	int termDay; 
-=======
-	/** Termination day of swap */
-	int termDay;
->>>>>>> f96fcd41573cfe6e97eacfbb1a36b3b04b51262c
 	/** Termination month of swap*/
 	int termMonth;
 	/** Termination year of swap*/
@@ -39,41 +29,51 @@ public class Swap {
 	double floatRateSpread;
 	/** Fixed rate*/
 	double fixedRate;
-	/** 1 = pays fixed rate
-	 *  -1 = pays floating rate
-	 */
+	/** Payer of Fixed leg*/
+	String PayerFixed;
+	/** Payer of Float leg*/
+	String PayerFloat;
 	/** the number of the trader */ 
 	int trader; 
-	int fixedOrfloat;
-	
 	Date date;
 	
-<<<<<<< HEAD
-	public Swap(int sDay, int sMonth, int sYear, int tDay, int tMonth, int tYear, double flRate, double spread, double fiRate, int t) {
-		startDay = sDay; 
-		startMonth = sMonth;
-		startYear = sYear;
-		termDay = tDay; 
-=======
-	public Swap(int sDay, int sMonth, int sYear, int tDay, int tMonth, int tYear, double flRate, double spread, double fiRate, int fixorfloat) {
+
+	/** Constructor of Swap class
+	 * @param sDay: startday
+	 * @param sMonth: startmonth
+	 * @param sYear: startyear
+	 * @param tDay: terminal day
+	 * @param tMonth: terminal month
+	 * @param tYear: terminal year
+	 * @param flRate: floatrate
+	 * @param spread: floatspread
+	 * @param fiRate: fixed rate
+	 * @param payerFloat: payer of float leg
+	 * @param payerFixed: payer of fixed leg
+	 * @param t: trader
+	 */
+	public Swap(int sDay, int sMonth, int sYear, int tDay, int tMonth, int tYear, double flRate, double spread, double fiRate, String payerFloat, String payerFixed, int t) {
+		date = new Date();
 		startDay = sDay;
 		startMonth = sMonth;
 		startYear = sYear;
+
 		termDay = tDay;
->>>>>>> f96fcd41573cfe6e97eacfbb1a36b3b04b51262c
 		termMonth = tMonth;
 		termYear = tYear;
+		
 		floatRate = flRate;
 		floatRateSpread = spread;
 		fixedRate = fiRate;
-<<<<<<< HEAD
-		int trader = t; 
-		date = new Date();
-=======
-		fixedOrfloat = fixorfloat;
->>>>>>> f96fcd41573cfe6e97eacfbb1a36b3b04b51262c
+
+		PayerFixed = payerFixed;
+		PayerFloat = payerFloat;
+		trader = t; 
 	}
 	
+	/**
+	 * Print swap trade
+	 */
 	void printSwap() {
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		System.out.println(dateFormat.format(date));
@@ -86,9 +86,12 @@ public class Swap {
 				 + floatRate    + " "
 				 + floatRateSpread  + " "
 				 + fixedRate	+ " "
-				 + fixedOrfloat);
+				 + PayerFloat + " "
+				 + PayerFixed + " trader"
+				 + trader);
 	}
 	
 	
+
 	
 }
